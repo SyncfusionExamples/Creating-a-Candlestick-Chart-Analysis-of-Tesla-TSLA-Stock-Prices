850cee13cf3fc57ce89d9b55eba35787e69f83f8
NitheeshKumarThangaraj <nitheeshkumar.t@syncfusion.com>
2023-12-07 15:50:45 +0530

    Changed the UI

--- CandlestickSample/MainPage.xaml.cs
+++ CandlestickSample/MainPage.xaml.cs
@@ -7,37 +7,83 @@ namespace CandlestickSample
         public MainPage()
         {
             InitializeComponent();
+
+            //trackball.LabelStyle = new ChartLabelStyle
+            //{
+            //    LabelFormat = "0.00",
+            //    Background = Colors.Black,
+            //    TextColor = Colors.White
+            //};
         }
 
-        private void segmentedControl_SelectionChanged(object sender, Syncfusion.Maui.Buttons.SelectionChangedEventArgs e)
+       
+        private void DayRange_Changed(object sender, EventArgs e)
+        {            
+            Label1.Background = Colors.LightSkyBlue;
+            Label2.Background = Colors.Transparent;
+            Label3.Background = Colors.Transparent;
+            Label4.Background = Colors.Transparent;
+
+            xAxis.AutoScrollingDeltaType = DateTimeIntervalType.Days;
+            xAxis.AutoScrollingDelta = 7;
+        }
+
+        private void WeekRange_Changed(object sender, EventArgs e)
         {
-            xAxis.AutoScrollingMode = Syncfusion.Maui.Charts.ChartAutoScrollingMode.Start;
+            Label1.Background = Colors.Transparent;
+            Label2.Background = Colors.LightSkyBlue;
+            Label3.Background = Colors.Transparent;
+            Label4.Background = Colors.Transparent;
+            xAxis.AutoScrollingDeltaType = DateTimeIntervalType.Days;
+            xAxis.AutoScrollingDelta = 15;
+        }
 
-            switch (e.NewIndex)
+        private void MonthRange_Changed(object sender, EventArgs e)
+        {
+            Label1.Background = Colors.Transparent;
+            Label2.Background = Colors.Transparent;
+            Label3.Background = Colors.LightSkyBlue;
+            Label4.Background = Colors.Transparent;
+            xAxis.AutoScrollingMode = Syncfusion.Maui.Charts.ChartAutoScrollingMode.End;
+            xAxis.AutoScrollingDeltaType = DateTimeIntervalType.Months;
+            xAxis.AutoScrollingDelta = 2;
+        }
+
+        private void YearRange_Changed(object sender, EventArgs e)
+        {
+            Label1.Background = Colors.Transparent;
+            Label2.Background = Colors.Transparent;
+            Label3.Background = Colors.Transparent;
+            Label4.Background = Colors.LightSkyBlue;
+            yAxis.Minimum = 90;
+            yAxis.Maximum = 320;
+            xAxis.AutoScrollingDeltaType = DateTimeIntervalType.Years;
+            xAxis.AutoScrollingDelta = 1;
+        }
+
+        int month = int.MaxValue;
+      
+        private void xAxis_LabelCreated(object sender, ChartAxisLabelEventArgs e)
+        {
+            DateTime baseDate = new(1899, 12, 30);
+            var date = baseDate.AddDays(e.Position);
+            if (date.Month != month)
             {
-                case 0:
-                    xAxis.AutoScrollingDeltaType = DateTimeIntervalType.Days;
-                    xAxis.AutoScrollingDelta = 7;
-                    break;
+                ChartAxisLabelStyle labelStyle = new();
+                labelStyle.LabelFormat = "yyyy";
+                labelStyle.FontAttributes = FontAttributes.Bold;
+                e.LabelStyle = labelStyle;
 
-                case 1:
-                    xAxis.AutoScrollingDeltaType = DateTimeIntervalType.Days;
-                    xAxis.AutoScrollingDelta = 15;
-                    break;
-
-                case 2:
-                    xAxis.AutoScrollingDeltaType = DateTimeIntervalType.Months;
-                    xAxis.AutoScrollingDelta = 4;
-                    break;
-
-                case 3:
-                    xAxis.AutoScrollingDeltaType = DateTimeIntervalType.Years;
-                    xAxis.AutoScrollingDelta = 1;
-                    break;
-
-                default:
-                    break;
+                month = date.Month;
+            }
+            else
+            {
+                ChartAxisLabelStyle labelStyle = new();
+                labelStyle.LabelFormat = "MMM-dd";
+                e.LabelStyle = labelStyle;
             }
         }
     }
+
+   
 }
